74da802658e7d1074b960e4d6c4452fc263f1772
Digvijay <rajputdigvijaysinh45@gmail.com>
2026-09-08 19:43:05 +0530

    practice

--- interview-Base/Q42.ts
+++ interview-Base/Q42.ts
@@ -37,3 +37,53 @@ for (let i = 1;i<=5; i++){
     }
     console.log(row);
 }
+
+// hollow triangle pattern 
+
+for (let i = 1;i<=5; i++){
+    let row = ""
+
+    for (let j = 1; j<=i; j++){
+        if(j === 1 || j === i || i === 5 ){
+            row = row + "*"
+        } else {
+            row = row + " "
+        }
+    }
+    console.log(row);
+}
+
+// reverse right angle 
+
+for(let i = 5; i>=1; i--){
+    let row = ""
+
+    for(let j = 1; j<=i; j++){
+        row = row + j
+    }
+    console.log(row);
+}
+
+// right angle number 
+for(let i = 1; i<=5; i++){
+    let row = ""
+
+    for(let j = 1; j<=i; j++){
+        row = row + i
+    }
+    console.log(row);
+}
+
+// flyorid trinagle 
+let num = 1;
+for(let i = 1; i<=4; i++){
+    let row = ""
+     
+  
+
+    for(let j = 1; j<=i; j++){
+        row = row + num + " "
+        num++
+    }
+    console.log(row);
+}
